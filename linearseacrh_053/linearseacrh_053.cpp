1e527cea8c0a0ffe52ee5f7b422e92ae76cdeec7
andiniseptia <andiniseptiandri124@gmail.com>
2023-04-14 23:19:25 +0700

    Langkah 5B tidak ditemukan

--- linearseacrh_053/linearseacrh_053.cpp
+++ linearseacrh_053/linearseacrh_053.cpp
@@ -52,3 +52,12 @@ void LinearSearch()
 				break;
 			}
 		}
+
+		if (i == n) // Langkah 5B not found
+			cout << "\n" << item << " not found in the array\n";
+		cout << "\nNumber of compaarisons : " << comparisons << endl;
+
+		cout << "\nContinue search (y/n) : ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'y'));
+}
